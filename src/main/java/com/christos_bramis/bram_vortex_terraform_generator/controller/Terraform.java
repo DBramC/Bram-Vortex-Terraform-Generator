38a5212d8d22b5a537e3e570b558492f9291e68d
DaBram <bramis04chris@gmail.com>
2026-03-17 21:27:13 +0200

    made controller

--- src/main/java/com/christos_bramis/bram_vortex_terraform_generator/controller/Terraform.java
+++ src/main/java/com/christos_bramis/bram_vortex_terraform_generator/controller/Terraform.java
@@ -1,4 +1,10 @@
 package com.christos_bramis.bram_vortex_terraform_generator.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/terraform")
 public class Terraform {
+
 }
