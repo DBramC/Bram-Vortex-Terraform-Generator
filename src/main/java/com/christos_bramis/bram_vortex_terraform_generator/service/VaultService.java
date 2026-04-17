1f48a064bff7e1cbc643a0e1974a27a5119aa65a
DaBram <bramis04chris@gmail.com>
2026-04-17 17:55:38 +0300

    fixed key override bug

--- src/main/java/com/christos_bramis/bram_vortex_terraform_generator/service/VaultService.java
+++ src/main/java/com/christos_bramis/bram_vortex_terraform_generator/service/VaultService.java
@@ -47,8 +47,10 @@ public class VaultService {
 
             System.out.println("🏦 [VAULT-SERVICE] Storing Private Key in Vault path: " + vaultPath);
 
-            vaultTemplate.write(vaultPath, Map.of("private_key", privateKeyPem));
-            vaultTemplate.write(vaultPath, Map.of("public_key", publicKeyOpenSSH));
+            vaultTemplate.write(vaultPath, Map.of(
+                    "private_key", privateKeyPem,
+                    "public_key", publicKeyOpenSSH
+            ));
 
 
             return publicKeyOpenSSH;
